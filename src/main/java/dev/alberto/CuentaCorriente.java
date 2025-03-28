b0e009b880dfd07e7b13eeaf968351b31f605e05
Algama17 <albertogarciamallada@gmail.com>
2025-03-28 14:34:59 +0100

    fifth code

--- src/main/java/dev/alberto/CuentaCorriente.java
+++ src/main/java/dev/alberto/CuentaCorriente.java
@@ -2,5 +2,14 @@ package dev.alberto;
 
 public class CuentaCorriente extends Cuenta{
 
+    private float sobregiro;
 
+    public CuentaCorriente(float saldo, float tasaAnual) {
+        super(saldo, tasaAnual);
+        this.sobregiro = 0;
+    }
+
+    public float getSobregiro() {
+        return sobregiro;
+    }
 }
