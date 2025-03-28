f587afdc4ec5f38738a335fb2ab6710de4e7cceb
Algama17 <albertogarciamallada@gmail.com>
2025-03-28 11:32:21 +0100

    first code

--- src/main/java/dev/alberto/Cuenta.java
+++ src/main/java/dev/alberto/Cuenta.java
@@ -2,4 +2,18 @@ package dev.alberto;
 
 public class Cuenta {
 
+    protected float saldo;
+    protected  float tasaAnual;
+    protected int numConsignaciones;
+    protected int numRetiros;
+    protected float comisionMensual;
+
+    public Cuenta(float saldo, float tasaAnual){
+        this.saldo = saldo;
+        this.tasaAnual = tasaAnual;
+        this.numConsignaciones = 0;
+        this.numRetiros = 0;
+        this.comisionMensual = 0;
+
+    };
 }
